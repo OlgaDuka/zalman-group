957c92d66b7d17a6f10318f9a387fd69ae0bdf21
Olga Duka <olgaduk@mail.ru>
2018-03-11 15:15:14 +0600

    Стилизация главной - мобильная версия

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,31 @@
 'use strict';
 (function () {
+  // ----------------------------------------------
+  // Меню
+  // ----------------------------------------------
+  var header = document.querySelector('.header');
+  var toggle = header.querySelector('.header__toggle');
+  var menu = header.querySelector('.menu');
+  var title = header.querySelector('.header__title');
+  var service = header.querySelector('.service');
+  // Закрываем меню, если JS работает
+  menu.classList.remove('menu--nojs');
+  menu.classList.add('menu--close');
+  toggle.classList.remove('header__toggle--collapse');
+  title.classList.remove('header__title--hidden');
+  service.classList.remove('service--mobile-menu-open');
+
+  // Переключаем состояние меню по кнопке и меняем высоту хедера
+  toggle.addEventListener('click', function () {
+    menu.classList.toggle('menu--close');
+    toggle.classList.toggle('header__toggle--collapse');
+    title.classList.toggle('header__title--hidden');
+    service.classList.toggle('service--mobile-menu-open');
+  });
+
+  // ----------------------------------------------
+  // Слайдер
+  // ----------------------------------------------
   var slides = document.querySelectorAll('.slider__slide');
   var sliderBtns = document.querySelectorAll('.slider__btn');
   var slideCurrentNum = 0;
